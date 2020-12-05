dd86c21c97db480c32fb5d17dc47994c5f9da989
Minioin <minioin@protonmail.com>
2020-12-05 13:42:13 +0545

    Add search logging by writing to a text file

--- bin/service.ts
+++ bin/service.ts
@@ -4,14 +4,21 @@ import { getSearchUrl } from "../src/mod.ts";
 
 const app = new Application();
 const config = await loadUserConfig();
+const home = Deno.env.get("HOME");
 
 app.get("/search", async (c) => {
   const query = c.queryParams["q"];
   if (query) {
-    let args = query.split(" ");
+    const loggingProxy = Deno.writeTextFile(
+      `${home}/.local/share/q/history.log`,
+      `${+new Date()}, ${query}\n`,
+      { append: true, create: true },
+    );
+    const args = query.split(" ");
     const { url, queryString } = await getSearchUrl(config, args);
     c.response.status = 307;
     c.response.headers.append("location", url.replace("%s", queryString));
+    await loggingProxy;
     return c.response;
   }
 
