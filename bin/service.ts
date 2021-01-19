f7c274eaefb46004983dbb54cd597e24ad40907f
Minioin <minioin@protonmail.com>
2021-01-19 21:53:07 +0545

    Use kv service for loggin

--- bin/service.ts
+++ bin/service.ts
@@ -4,23 +4,28 @@ import { getSearchUrl } from "../src/mod.ts";
 
 const app = new Application();
 const config = await loadUserConfig();
-const home = Deno.env.get("HOME");
 
 app.get("/search", async (c) => {
   const query = c.queryParams["q"];
   if (query) {
-    const loggingProxy = Deno.writeTextFile(
-      `${home}/.local/share/q/history.log`,
-      `${+new Date()}, ${query}\n`,
-      { append: true, create: true },
-    );
+    const logSearch = fetch("http://localhost:3737/event/default", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        event: "searched",
+        data: JSON.stringify(query),
+      }),
+    });
     const args = query.split(" ");
     const { url, queryString } = await getSearchUrl(config, args);
     c.response.status = 307;
     c.response.headers.append("location", url.replace("%s", queryString));
-    await loggingProxy;
+    await logSearch;
     return c.response;
   }
 
   return "Hello, there";
-}).start({ port: 3738 });
+})
+.start({ port: 3738 });
